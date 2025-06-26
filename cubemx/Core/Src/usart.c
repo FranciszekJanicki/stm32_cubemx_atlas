62adc168d7670b847c3d7b322be2cf62828282b4
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-06-26 02:25:51 +0200

    changes

--- cubemx/Core/Src/usart.c
+++ cubemx/Core/Src/usart.c
@@ -123,5 +123,11 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 }
 
 /* USER CODE BEGIN 1 */
-
+int _write(int file, uint8_t* p, int len)
+{
+    if (HAL_UART_Transmit(&huart2, p, len, len) == HAL_OK) {
+        return len;
+    }
+    return 0;
+}
 /* USER CODE END 1 */
